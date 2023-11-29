29e89828dba776a2cfc4a0dda40b2960c0123a15
slowliver <fiume.nome@gmail.com>
2023-11-29 19:36:52 +0900

    [fix] Engine と Editor を分離

---
+++ Source/Editor/Main.cpp
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+#include <windows.h>
+
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+	return MessageBoxA(NULL, "hello, world", "caption", 0);
+}
--- Source/Engine/Framework/Application.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#include <windows.h>
-
-int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
-{
-	return MessageBox(NULL, "hello, world", "caption", 0);
-}
