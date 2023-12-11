81a11e6b224d06dc776f72bf084b28141ccb5043
slowliver <fiume.nome@gmail.com>
2023-12-11 22:27:57 +0900

    [Core] Allocator fix

---
+++ Include/Engine/Core/Allocator.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <type_traits>
+
+#include <Engine/Core/CoreMinimal.h>
+
+#if STRAVA_WINDOWS
+#include <Windows.h>
+#endif
+
+namespace StravaEngine::Core
+{
+namespace Internal
+{
+template<class Type, Size k_alignment>
+struct Allocator
+{
+	using value_type = Type;
+	using propagate_on_container_move_assignment = std::true_type;
+	using size_type = Size;
+	using difference_type = std::ptrdiff_t;
+	using is_always_equal = std::true_type;
+
+	Allocator() {};
+	Allocator(const Allocator&) {};
+	template<typename OtherType, Size k_alignment2>
+	Allocator(const Allocator<OtherType, k_alignment2>&) {};
+	~Allocator() {};
+
+	Allocator& operator=(const Allocator&) {};
+	template<typename OtherType, Size k_alignment2>
+	Allocator& operator=(const Allocator<OtherType, k_alignment2>&) {};
+
+	[[nodiscard]]
+	Type* allocate(size_type n)
+	{
+#if STRAVA_WINDOWS
+		return static_cast<Type*>(::_aligned_malloc(n * sizeof(Type), k_alignment));
+#else
+#error
+#endif
+	}
+
+	void deallocate(Type* p, size_type n)
+	{
+#if STRAVA_WINDOWS
+		::_aligned_free(p);
+#else
+#error
+#endif
+	}
+};
+
+template<class Type1, Size k_alignment1, class Type2, Size k_alignment2>
+inline bool operator==(const Allocator<Type1, k_alignment1>&, const Allocator<Type2, k_alignment2>&)
+{
+	return std::is_same_v<Type1, Type2>&& k_alignment1 == k_alignment2;
+}
+
+template<class Type1, Size k_alignment1, class Type2, Size k_alignment2>
+inline bool operator!=(const Allocator<Type1, k_alignment1>& lhs, const Allocator<Type2, k_alignment2>& rhs)
+{
+	return !(lhs == rhs);
+}
+}
+
+template<class Type, Size k_alignment = alignof(Type) < alignof(void*) ? alignof(void*) : alignof(Type)>
+using Allocator = Internal::Allocator<Type, k_alignment>;
+}
