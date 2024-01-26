69ce40918664b676bcba5d916a1f82fe2a87f55f
slowliver <fiume.nome@gmail.com>
2024-01-27 02:13:55 +0900

    [Add] MSVC

--- Source/Tool/Bootstrap/Program.cs
+++ Source/Tool/Bootstrap/Program.cs
@@ -9,6 +9,8 @@ using System.Collections.Specialized;
 using System.Net;
 using System.Security.Policy;
 using System.IO.Compression;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace Bootstrap
 {
@@ -70,6 +72,78 @@ namespace Bootstrap
 					}
 				}
 			}
+
+			// MSVC
+			{
+				var msvcDirectory = packageDirectory + "Microsoft\\MSVC\\";
+				if (!Directory.Exists(msvcDirectory))
+				{
+					Directory.CreateDirectory(msvcDirectory);
+					string vswhereOutput = null;
+					string clwhereOutput = null;
+					var vswherePath = packageDirectory + "Microsoft\\vswhere\\vswhere.exe";
+					if (File.Exists(vswherePath))
+					{
+						using (var vswhereProcess = new Process())
+						{
+							vswhereProcess.StartInfo.FileName = vswherePath;
+							vswhereProcess.StartInfo.Arguments = "-latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath";
+							vswhereProcess.StartInfo.UseShellExecute = false;
+							vswhereProcess.StartInfo.RedirectStandardOutput = true;
+							vswhereProcess.Start();
+							vswhereOutput = vswhereProcess.StandardOutput.ReadLine();
+							vswhereProcess.WaitForExit();
+						}
+					}
+					if (!string.IsNullOrEmpty(vswhereOutput))
+					{
+						var vcvarsallPath = Path.Combine(vswhereOutput, "VC\\Auxiliary\\Build\\vcvarsall.bat");
+						var clwherePath = temporaryDirectory + "clwhere.bat";
+						if (File.Exists(vcvarsallPath))
+						{
+							var batch = "";
+							batch += "@echo off\n";
+							batch += "setlocal\n";
+							batch += "call \"" + Path.Combine(vswhereOutput, "VC\\Auxiliary\\Build\\vcvarsall.bat") + "\" amd64 > nul 2>&1\n";
+							batch += "where cl\n";
+							File.WriteAllText(clwherePath, batch);
+						}
+						if (File.Exists(clwherePath))
+						{
+							using (var clwhereProcess = new Process())
+							{
+								clwhereProcess.StartInfo.FileName = clwherePath;
+								clwhereProcess.StartInfo.UseShellExecute = false;
+								clwhereProcess.StartInfo.RedirectStandardOutput = true;
+								clwhereProcess.Start();
+								clwhereOutput = clwhereProcess.StandardOutput.ReadLine();
+								clwhereProcess.WaitForExit();
+							}
+						}
+					}
+					if (!string.IsNullOrEmpty(clwhereOutput) && File.Exists(clwhereOutput))
+					{
+						Action<DirectoryInfo, DirectoryInfo> copyFiles = null;
+						copyFiles = (dst, src) =>
+						{
+							if (!dst.Exists)
+							{
+								dst.Create();
+							}
+							foreach (var file in src.GetFiles())
+							{
+								file.CopyTo(Path.Combine(dst.FullName, file.Name));
+							}
+							foreach (var dir in src.GetDirectories())
+							{
+								copyFiles(new DirectoryInfo(Path.Combine(dst.FullName, dir.Name)), dir);
+							}
+						};
+						copyFiles(new DirectoryInfo(msvcDirectory), new DirectoryInfo(Path.GetDirectoryName(clwhereOutput)));
+					}
+				}
+			}
+
 			// DXC
 			{
 				var dxcZipPath = temporaryDirectory + "DirectXShaderCompiler.zip";
