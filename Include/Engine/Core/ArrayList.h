be166c01565e46da95b2ecd145fe42b61e01affd
Yuki Kawana <fiume.nome@gmail.com>
2025-01-10 23:30:09 +0900

    [fix] Size to UInt32

--- Include/Engine/Core/ArrayList.h
+++ Include/Engine/Core/ArrayList.h
@@ -27,7 +27,7 @@ public:
 		, m_index(0)
 	{}
 
-	explicit ArrayListConstIterator(pointer pointer, Size offset = 0)
+	explicit ArrayListConstIterator(pointer pointer, UInt32 offset = 0)
 		: m_pointer(pointer)
 		, m_index(offset)
 	{}
@@ -93,7 +93,7 @@ public:
 
 	ArrayListConstIterator& operator+=(const difference_type offset)
 	{
-		m_index += static_cast<Size>(offset);
+		m_index += static_cast<UInt32>(offset);
 		return *this;
 	}
 	// End forward any ops.
@@ -152,7 +152,7 @@ public:
 
 private:
 	pointer m_pointer;
-	Size m_index;
+	UInt32 m_index;
 };
 
 template <class Type>
@@ -169,7 +169,7 @@ public:
 	ArrayListIterator()
 	{}
 
-	explicit ArrayListIterator(pointer pointer, Size offset = 0)
+	explicit ArrayListIterator(pointer pointer, UInt32 offset = 0)
 		: ArrayListConstIterator<Type>(pointer, offset)
 	{}
 
@@ -261,7 +261,7 @@ public:
 	using const_pointer = std::add_pointer_t<const Type>;
 	using reference = Type&;
 	using const_reference = const Type&;
-	using size_type = Size;
+	using size_type = UInt32;
 	using difference_type = std::ptrdiff_t;
 	using iterator = ArrayListIterator<Type>;
 	using const_iterator = ArrayListConstIterator<Type>;
@@ -269,8 +269,8 @@ public:
 	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
 	ArrayList() {}
-	explicit ArrayList(Size count) { Resize(count); }
-	ArrayList(Size count, const Type& value) { Resize(count, value); }
+	explicit ArrayList(UInt32 count) { Resize(count); }
+	ArrayList(UInt32 count, const Type& value) { Resize(count, value); }
 	template <class IteratorType>
 	ArrayList(IteratorType first, IteratorType last) { AddRange(first, last); }
 	ArrayList(const ArrayList& arrayList)
@@ -281,7 +281,7 @@ public:
 		{
 			auto allocator = TAllocator<Type>();
 			m_data = allocator.Allocate(m_capacity);
-			for (Size i = 0; i < m_count; ++i)
+			for (UInt32 i = 0; i < m_count; ++i)
 			{
 				::new(&m_data[i]) Type(arrayList.m_data[i]);
 			}
@@ -333,7 +333,7 @@ public:
 	const_reverse_iterator crbegin() const { return rbegin(); }
 	const_reverse_iterator crend() const { return rend(); }
 
-	void Reserve(Size capacity)
+	void Reserve(UInt32 capacity)
 	{
 		if (capacity <= m_capacity)
 		{
@@ -341,7 +341,7 @@ public:
 		}
 		auto allocator = TAllocator<Type>();
 		auto* newData = allocator.Allocate(capacity);
-		for (Size i = 0; i < m_count; ++i)
+		for (UInt32 i = 0; i < m_count; ++i)
 		{
 			::new(&newData[i]) Type(std::move(m_data[i]));
 			m_data[i].~Type();
@@ -354,18 +354,18 @@ public:
 		m_capacity = capacity;
 	}
 
-	Size GetCapacity() const { return m_capacity; }
+	UInt32 GetCapacity() const { return m_capacity; }
 
-	Size GetCount() const { return m_count; }
+	UInt32 GetCount() const { return m_count; }
 
-	Size GetMaxCount() const { return UINT64_MAX; }
+	UInt32 GetMaxCount() const { return UINT64_MAX; }
 
-	void Resize(Size count)
+	void Resize(UInt32 count)
 	{
 		Reserve(count);
 		if (count > m_count)
 		{
-			for (Size i = m_count; i < count; ++i)
+			for (UInt32 i = m_count; i < count; ++i)
 			{
 				::new(&m_data[i]) Type();
 			}
@@ -373,7 +373,7 @@ public:
 		}
 		else if (count < m_count)
 		{
-			for (Size i = count; i < m_count; ++i)
+			for (UInt32 i = count; i < m_count; ++i)
 			{
 				m_data[i].~Type();
 			}
@@ -381,12 +381,12 @@ public:
 		}
 	}
 
-	void Resize(Size count, const Type& value)
+	void Resize(UInt32 count, const Type& value)
 	{
 		Reserve(count);
 		if (count > m_count)
 		{
-			for (Size i = m_count; i < count; ++i)
+			for (UInt32 i = m_count; i < count; ++i)
 			{
 				::new(&m_data[i]) Type(value);
 			}
@@ -394,7 +394,7 @@ public:
 		}
 		else if (count < m_count)
 		{
-			for (Size i = count; i < m_count; ++i)
+			for (UInt32 i = count; i < m_count; ++i)
 			{
 				m_data[i].~Type();
 			}
@@ -422,7 +422,7 @@ public:
 			return;
 		}
 		auto* newData = allocator.Allocate(m_count);
-		for (Size i = 0; i < m_count; ++i)
+		for (UInt32 i = 0; i < m_count; ++i)
 		{
 			::new(&newData[i]) Type(std::move(m_data[i]));
 			m_data[i].~Type();
@@ -432,10 +432,10 @@ public:
 		m_capacity = m_count;
 	}
 
-	Type& GetAt(Size index) { return m_data[index]; }
-	const Type& GetAt(Size index) const { return m_data[index]; }
-	Type& operator[](Size index) { return GetAt(index); }
-	const Type& operator[](Size index) const { return GetAt(index); }
+	Type& GetAt(UInt32 index) { return m_data[index]; }
+	const Type& GetAt(UInt32 index) const { return m_data[index]; }
+	Type& operator[](UInt32 index) { return GetAt(index); }
+	const Type& operator[](UInt32 index) const { return GetAt(index); }
 
 	Type* GetData() { return m_data; }
 	const Type* GetData() const { return m_data; }
@@ -446,15 +446,15 @@ public:
 	const Type& GetBack() const { return m_data[m_count - 1]; }
 
 	template <class... Args>
-	Type& Emplace(Size index, Args&&... args)
+	Type& Emplace(UInt32 index, Args&&... args)
 	{
-		const Size oldCount = m_count;
-		const Size newCount = m_count + 1;
+		const UInt32 oldCount = m_count;
+		const UInt32 newCount = m_count + 1;
 		if (newCount > m_capacity)
 		{
 			Reserve(newCount * 3 / 2);
 		}
-		for (Size i = oldCount; i < index; --i)
+		for (UInt32 i = oldCount; i < index; --i)
 		{
 			::new(&m_data[i]) Type(std::move(m_data[i - 1]));
 		}
@@ -463,58 +463,58 @@ public:
 		return m_data[index];
 	}
 
-	void Insert(Size index, const Type& value) { Emplace(index, value); }
-	void Insert(Size index, Type&& value) { Emplace(index, std::move(value)); }
+	void Insert(UInt32 index, const Type& value) { Emplace(index, value); }
+	void Insert(UInt32 index, Type&& value) { Emplace(index, std::move(value)); }
 
 	template <class... Args>
 	Type& EmplaceBack(Args&&... args) { return Emplace(m_count, std::forward<Args>(args)...); }
 
-	Size Add(const Type& value) { EmplaceBack(value); return m_count - 1; }
-	Size Add(const Type&& value) { EmplaceBack(std::move(value)); return m_count - 1; }
+	UInt32 Add(const Type& value) { EmplaceBack(value); return m_count - 1; }
+	UInt32 Add(const Type&& value) { EmplaceBack(std::move(value)); return m_count - 1; }
 
 	template <class IteratorType>
-	void InsertRange(Size index, IteratorType first, IteratorType last)
+	void InsertRange(UInt32 index, IteratorType first, IteratorType last)
 	{
-		const Size numRangeCount = static_cast<Size>(last - first);
-		const Size oldCount = m_count;
-		const Size newCount = m_count + numRangeCount;
+		const UInt32 numRangeCount = static_cast<UInt32>(last - first);
+		const UInt32 oldCount = m_count;
+		const UInt32 newCount = m_count + numRangeCount;
 		if (newCount > m_capacity)
 		{
 			Reserve(newCount * 3 / 2);
 		}
 #if 0
 		Resize(newCount);
-		for (Size i = oldCount; i > index; --i)
+		for (UInt32 i = oldCount; i > index; --i)
 		{
 			m_data[i - 1 + numRangeCount] = std::move(m_data[i - 1]);
 		}
 #else
-		for (Size i = oldCount; i > index; --i)
+		for (UInt32 i = oldCount; i > index; --i)
 		{
 			::new(&m_data[i - 1 + numRangeCount]) Type(std::move(m_data[i - 1]));
 		}
 #endif
 		for (IteratorType itr = first; itr < last; ++itr)
 		{
-			const auto offset = static_cast<Size>(itr - first);
+			const auto offset = static_cast<UInt32>(itr - first);
 			::new(&m_data[index + offset]) Type(*itr);
 		}
 		m_count = newCount;
 	}
-	void InsertRange(Size index, const ArrayList& other) { InsertRange(index, other.cbegin(), other.cend()); }
-	void InsertRange(Size index, ArrayList&& other) { InsertRange(index, other.cbegin(), other.cend()); }
-	void InsertRange(Size index, std::initializer_list<Type> other) { InsertRange(index, other.begin(), other.end()); }
-	void InsertRange(Size index, Type* other, Size count) { InsertRange(index, &other[0], &other[count]); }
-	template <Size k_size>
-	void InsertRange(Size index, const Array<Type, k_size>& other) { InsertRange(index, other.cbegin(), other.cend()); }
+	void InsertRange(UInt32 index, const ArrayList& other) { InsertRange(index, other.cbegin(), other.cend()); }
+	void InsertRange(UInt32 index, ArrayList&& other) { InsertRange(index, other.cbegin(), other.cend()); }
+	void InsertRange(UInt32 index, std::initializer_list<Type> other) { InsertRange(index, other.begin(), other.end()); }
+	void InsertRange(UInt32 index, Type* other, UInt32 count) { InsertRange(index, &other[0], &other[count]); }
+	template <UInt32 k_size>
+	void InsertRange(UInt32 index, const Array<Type, k_size>& other) { InsertRange(index, other.cbegin(), other.cend()); }
 
 	template <class IteratorType>
 	void AddRange(IteratorType first, IteratorType last) { InsertRange(m_count, first, last); }
 	void AddRange(const ArrayList& other) { InsertRange(m_count, other); }
 	void AddRange(ArrayList&& other) { InsertRange(m_count, std::move(other)); }
 	void AddRange(std::initializer_list<Type> other) { InsertRange(m_count, other); }
-	void AddRange(Type* other, Size count) { InsertRange(m_count, other, count); }
-	template <Size k_size>
+	void AddRange(Type* other, UInt32 count) { InsertRange(m_count, other, count); }
+	template <UInt32 k_size>
 	void AddRange(const Array<Type, k_size>& other) { InsertRange(m_count, other); }
 
 	void Clear() { Resize(0); }
@@ -526,17 +526,22 @@ public:
 		std::swap(m_capacity, other.m_capacity);
 	}
 
+	bool Contains(Type& element)
+	{
+
+	}
+
 	// C++ STL alias
-	Size size() const { return GetCount(); }
-	Size max_size() const { return GetMaxCount(); }
-	void resize(Size count) { Resize(count); }
-	void resize(Size count, const Type& value) { Resize(count, value); }
-	Size capacity() const { return GetCapacity(); }
+	UInt32 size() const { return GetCount(); }
+	UInt32 max_size() const { return GetMaxCount(); }
+	void resize(UInt32 count) { Resize(count); }
+	void resize(UInt32 count, const Type& value) { Resize(count, value); }
+	UInt32 capacity() const { return GetCapacity(); }
 	bool empty() const { bool IsEmpty(); }
-	void reserve(Size count) { Reserve(count); }
+	void reserve(UInt32 count) { Reserve(count); }
 	void shrink_to_fit() { TrimToSize(); }
-	Type& at(Size index) { return GetAt(index); }
-	const Type& at(Size index) const { return GetAt(index); }
+	Type& at(UInt32 index) { return GetAt(index); }
+	const Type& at(UInt32 index) const { return GetAt(index); }
 	Type* data() { return GetData(); }
 	const Type* data() const { return GetData(); }
 	Type& front() { return GetFront(); }
@@ -548,18 +553,18 @@ public:
 	template <class... Args> void emplace_back(Args&&... args) { EmplaceBack(std::forward<Args>(args)...); }
 	template <class... Args> Type& emplace_back(Args&&... args) { return EmplaceBack(std::forward<Args>(args)...); }
 	void pop_back() { Resize(m_count - 1); }
-	iterator insert(const_iterator position, const Type& value) { Insert(static_cast<Size>(position - cbegin()), value); }
-	iterator insert(const_iterator position, Type&& value) { Insert(static_cast<Size>(position - cbegin()), std::move(value)); }
-	iterator insert(const_iterator position, Size count, const Type& value);
-	iterator insert(const_iterator position, std::initializer_list<Type> other) { Insert(static_cast<Size>(position - cbegin()), other); }
-	template <class IteratorType> iterator insert(IteratorType position, IteratorType first, IteratorType last) { Insert(static_cast<Size>(position - cbegin()), first, last); }
-	template <class... Args> iterator emplace(const_iterator position, Args&&... args) { Emplace(static_cast<Size>(position - cbegin()), std::forward<Args>(args)); }
+	iterator insert(const_iterator position, const Type& value) { Insert(static_cast<UInt32>(position - cbegin()), value); }
+	iterator insert(const_iterator position, Type&& value) { Insert(static_cast<UInt32>(position - cbegin()), std::move(value)); }
+	iterator insert(const_iterator position, UInt32 count, const Type& value);
+	iterator insert(const_iterator position, std::initializer_list<Type> other) { Insert(static_cast<UInt32>(position - cbegin()), other); }
+	template <class IteratorType> iterator insert(IteratorType position, IteratorType first, IteratorType last) { Insert(static_cast<UInt32>(position - cbegin()), first, last); }
+	template <class... Args> iterator emplace(const_iterator position, Args&&... args) { Emplace(static_cast<UInt32>(position - cbegin()), std::forward<Args>(args)); }
 	void swap(ArrayList other) { Swap(other); }
 	void clear() { Clear(); }
 
 private:
 	Type* m_data = nullptr;
-	Size m_count = 0;
-	Size m_capacity = 0;
+	UInt32 m_count = 0;
+	UInt32 m_capacity = 0;
 };
 }
